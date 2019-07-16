a0e7e0d863e4fb7389f12aa277c4bc6117ed11e2
Beer van der Drift <beerdrift@gmail.com>
2019-07-16 14:56:37 +0200

    feat: add backend project

---
+++ backend/src/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const port = 3000;
+
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello World"));
+
+app.listen(port, () => console.log("App is listening on port " + port));
